8cc6662cf63f1091aed2b560e45c9a8d8639e707
piensaenpixel <piensaenpixel@gmail.com>
2018-09-15 23:52:39 +0200

    update app

--- app/src/routes/AnimalMap/components/Panels.jsx
+++ app/src/routes/AnimalMap/components/Panels.jsx
@@ -4,28 +4,28 @@ import './panels.css';
 export default class Sidebar extends Component {
   render() {
     return (
-      <div className="as-panel as-panel--top as-panel--right as-panel--vertical">
-        <div className="as-panel__element as-bg--support-01">
-          <h1 className="as-title">{this.props.name}</h1>
-          <h2 className="as-subheader">{this.props.sname}</h2>
+      <div className="app-map">
+        <div className="as-panel as-panel--top as-panel--right as-panel--vertical">
+          <div className="as-panel__element">
+            <div class="app-map_header">
+              <h2 className="as-subheader">{this.props.sname}</h2>
+              <div class="app-map_headerTitle">
+                <h1 className="as-title">{this.props.name}</h1>
+              </div>
+            </div>
+            <span className="as-badge as-bg--warning"> {this.props.status}</span>
+            <p className="app-map_text">
+              {this.props.text}
+            </p>
+            <dl class="app-map_list">
+              <dt className="as-subheader">Population:</dt>
+              <dd className="as-body">{this.props.population}</dd>
+              <dt className="as-subheader">Habitat Loss:</dt>
+              <dd className="as-body">{this.props.overlap} % </dd>
+            </dl>
+          </div>
         </div>
-
-        <div className="as-panel__element">
-          <h2 className="as-subheader">Descripción</h2>
-          <p className="as-body">
-            {this.props.text}
-          </p>
-        </div>
-
-        <div className="as-panel__element">
-          <h2 className="as-subheader">Info</h2>
-          <p className="as-badge as-bg--warning"> {this.props.status} </p>
-          <h2 className="as-subheader">Population:</h2>
-          <p className="as-body">{this.props.population}</p>
-          <h2 className="as-subheader">Habitat Loss:</h2>
-          <p className="as-body">{this.props.overlap} %</p>
-        </div>
-      </div >
+      </div>
     );
   }
 }
