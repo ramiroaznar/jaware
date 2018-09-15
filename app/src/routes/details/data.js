b0033b50550cd551e82da1e573e035fac9dfcff3
IagoLast <iago.lastra@gmail.com>
2018-09-15 13:12:19 +0200

    Update details page

---
+++ app/src/routes/details/data.js
@@ -0,0 +1,37 @@
+export default {
+  1234: {
+    name: 'Nocilla',
+    type: 'warning',
+    img_url: 'https://static.condisline.com/resize_1280x1024/images/catalog/large/207523.jpg',
+    badges: [{
+        type: 'as-badge as-bg--badge-green',
+        text: 'Reciclable'
+      },
+      {
+        type: 'as-badge as-bg--badge-blue',
+        text: 'Auténtica'
+      },
+      {
+        type: 'as-badge as-bg--warning',
+        text: 'Insano'
+      },
+      {
+        type: 'as-badge as-bg--error',
+        text: 'Defaunación'
+      },
+    ],
+    eco_info: `Debido a la gran demanda internacional, las plantaciones de palmera de aceite están reemplazando
+    rápidamente el hábitat de los orangutanes en la selva, que se encuentran en peligro crítico,
+   puesto que más del 90% de su hábitat ha sido destruido en los últimos 20 años.`,
+    afected_animals: [{
+        name: 'Orangutan',
+        href: ''
+      },
+      {
+        name: 'jaguar',
+        href: ''
+      },
+    ]
+  },
+
+}
--- app/src/routes/details/index.jsx
+++ app/src/routes/details/index.jsx
@@ -1,71 +1,44 @@
 import React, { Component } from 'react';
 import Banner from './components/Banner';
+import DATA from './data';
 
 import './details.css';
 
 export default class Details extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      type: 'danger',
-      name: 'Nocilla',
-      img_url: 'https://static.condisline.com/resize_1280x1024/images/catalog/large/207523.jpg',
-      badges: [
-        {
-          type: 'as-badge as-bg--badge-greem',
-          text: 'Reciclable'
-        },
-        {
-          type: 'as-badge as-bg--badge-blue',
-          text: 'Auténtica'
-        },
-        {
-          type: 'as-badge as-bg--badge-warning',
-          text: 'Insano'
-        },
-        {
-          type: 'as-badge as-bg--badge-error',
-          text: 'Defaunación'
-        },
-      ]
-    }
+    this.state = DATA[props.match.params.id]
   }
 
   render() {
+    if (!this.state) {
+      return <h1 className="as-jumbo">Producto no encontrado</h1>
+    }
     return (
       <main className="app-details">
         <Banner type={this.state.type}></Banner>
-
         <div className="details__img-wrapper">
           <img className="details__img" src={this.state.img_url} alt={this.state.name} />
         </div>
 
-        <div className="details__data">
+        <div className="details__section">
           <h1 className="as-title">{this.state.name}</h1>
           <div className="details__labels">
-            {/* {this.state.badges.map((label, index) => <span key={index} className={label.type}> {label.text} </span>)} */}
-            <span className="as-badge as-bg--badge-green">Reciclable</span>
-            <span className="as-badge as-bg--badge-blue"> Auténtica</span>
-            <span className="as-badge as-bg--warning">Insano</span>
-            <span className="as-badge as-bg--error">Defaunación</span>
-          </div>
-          <div className="details__section">
-            <h3 className="as-subheader">Impacto ecológico</h3>
-            <p className="as-body">
-              Debido a la gran demanda internacional, las plantaciones de palmera de aceite están reemplazando
-              rápidamente el hábitat de los <a href="">orangutanes</a> en la selva, que se encuentran en peligro crítico,
-             puesto que más del 90% de su hábitat ha sido destruido en los últimos 20 años.
-           </p>
-          </div>
-          <div className="details__section">
-            <h3 className="as-subheader">Animales afectados</h3>
-            <ul className="as-list">
-              <li className="as-list__item"><a href="">Orangutan</a></li>
-              <li className="as-list__item"><a href="">Jaguar</a></li>
-            </ul>
+            {this.state.badges.map((label, index) => <span key={index} className={label.type}> {label.text} </span>)}
           </div>
         </div>
 
+        <div className="details__section">
+          <h3 className="as-subheader">Impacto ecológico</h3>
+          <p className="as-body">{this.state.eco_info}</p>
+        </div>
+
+        <div className="details__section">
+          <h3 className="as-subheader">Animales afectados</h3>
+          <ul className="as-list">
+            {this.state.afected_animals.map((animal, index) => <li key={index}  className="as-list__item"><a href={animal.href}>{animal.name}</a></li>)}
+          </ul>
+        </div>
       </main>
     );
   }
