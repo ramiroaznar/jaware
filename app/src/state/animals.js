bd798eae87235f980986a6397790d6d2daeca975
IagoLast <iago.lastra@gmail.com>
2018-09-16 11:38:16 +0200

    Add animals

---
+++ app/src/state/animals.js
@@ -0,0 +1,33 @@
+export default [{
+    name: 'Asian elephant',
+    value: 18.73
+  },
+  {
+    name: 'Sumatran orangutan',
+    value: 9.62
+  },
+  {
+    name: 'Bornean orangutan',
+    value: 67.28,
+  },
+  {
+    name: 'Sumatran rhinoceros',
+    value: 79.57,
+  },
+  {
+    name: 'Sun bear',
+    value: 3.56
+  },
+  {
+    name: 'Tapanuli orangutan',
+    value: 94.76
+  },
+  {
+    name: 'Malayan tapir',
+    value: 13.69
+  },
+  {
+    name: 'Sunda pangolin',
+    value: 93.07
+  }
+];
